1b2e3e1f7a704d769f767c328033a7ace07b443f
swarnavdas15 <swarnavofcl15@gmail.com>
2024-10-22 23:56:01 +0530

    Week List

---
+++ day7.py
@@ -0,0 +1,14 @@
+#DAy7
+#enamulating do while loop
+#Week list
+list =["Monday","Tuesday","Wednessday","Thursday","Friday","Saturday","Sunday"]
+
+#Printing days in loop
+
+for day in list:
+    print(day)
+    if (day=="Sunday"):
+        print(day)
+        break;
+#print loop break statement
+print("\nWeek Over\n")
