9523f159b6dc8a1ff5bcc9706d4a7c6f5caf0bae
Swarnav Das <164478477+swarnavdas15@users.noreply.github.com>
2024-10-31 20:24:00 +0530

    string method

---
+++ day5.py
@@ -0,0 +1,25 @@
+#day5
+# String methods
+
+data="Hey guys, this is swarnav  here .And this is the 28days of learning python .today we are in day 5 and we performing String methods   "
+print("4", data.capitalize())
+print("5",data.upper())
+print("6",data.lower())
+print("7",data.endswith("methods"))
+print("8",data.startswith("TO"))
+print("9",data.title())
+print("10",data.isalnum())
+print("11",data.rstrip("methods"))
+print("12",data.split())
+print("13",data.strip("methods"))
+print("14",data.center(50))
+print("15",data.count("methods"))
+print("16",data.find("methods"))
+print("17",data.index("methods"))
+print("18",data.isalpha())
+print("19",data.isspace())
+print("20",data.isprintable())
+print("21",data.isupper())
+print("22",data.swapcase())
+print("23",data.replace("swarnav", "SWarnav Das"))
+# split strip center coount find replace index isalpha isprintable isspace issuper swapcase
